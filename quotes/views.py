1a747f83efca840e9fff51539d83b3bbad01b9ad
angelmaria <angelmaria75@gmail.com>
2024-07-23 12:18:06 +0200

    configuración avanzada proyecot Django. Queda configurar base de datos y scripts

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Quote, Author
 
-# Create your views here.
+def quote_list(request):
+    quotes = Quote.objects.all()
+    return render(request, 'quotes/quote_list.html', {'quotes': quotes})
+
+def author_detail(request, author_id):
+    author = Author.objects.get(id=author_id)
+    quotes = author.quote_set.all()
+    return render(request, 'quotes/author_detail.html', {'author': author, 'quotes': quotes})
