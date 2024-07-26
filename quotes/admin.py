6a1aa486a772a0cc994fa6038634d1213239ca41
angelmaria <angelmaria75@gmail.com>
2024-07-26 11:29:35 +0200

    preparando rama para selenium

--- quotes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Author, Quote, Tag, QuoteTag
-
-admin.site.register(Author)
-admin.site.register(Quote)
-admin.site.register(Tag)
-admin.site.register(QuoteTag)
